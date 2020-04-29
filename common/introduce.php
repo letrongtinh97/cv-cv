33083f426b2a751dbc02a3fa504aca780c5caaf5
LE TRONG TINH <letrongtinhtdnu97@gmail.com>
2020-04-29 17:05:18 +0700

    Update file

---
+++ common/introduce.php
@@ -0,0 +1,12 @@
+<?php
+
+    
+    $introduce = array(
+        "introduce-yourself"  =>  "Giới thiệu bản thân", 
+        "title"   =>  "CV - Lê Trọng Tình", 
+        "JS"    =>  "5 tuần", 
+        "PHP"   =>  "3 tuần"
+    );
+
+
+?>
